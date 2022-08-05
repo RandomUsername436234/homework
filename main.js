bdcfe1ea76fc3a83f638e483e4955edbb4a711e4
unknown <shafferjarrod99@aol.com>
2022-08-04 18:00:53 -0700

    work

--- main.js
+++ main.js
@@ -1,40 +1,12 @@
-console.log("hello world");
-for (let i = 0; i <= 20; i++) {
- if (i % 2 !== 0) console,log(1);
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', () => {
+    const toast = new bootstrap.Toast(toastLiveExample)
+
+    toast.show()
+  })
 }
-const Gameboy = ["Gameboy", "wolf", 16, "Gotham City"]
-const sharky = ["Sharky", "shark", 20, "Left Coast"]
-const plantee = ["Plantee", "plant",  5001 , "Mordor"]
-const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
-const dart = ["D'Art" , "Hawkins" , "Demogorgan Dog", 2, "Upside Down"]
-
-const Leonardo = {"Blue"}
-const Donatello ={"Purple"}
-const Raphael ={"Red"}
-const Michaelangelo = {"Orange"}
-
-
-const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
-console.log("titanic");
-.sort
-push("Guardians Of The Galaxy")
-shift("Fast and Furious")
-unshift
-splice ("Django Unchained")
-
-const whereIsWaldo = [["Timmy", "Frank"], 
-                    ["Lucinda", "Jacc", "No One", "Snoop"],
-                    ["Petunia", ["Baked Goods", "Waldo"]]];
-console.log("Waldo");
-
-console.log("Love me, Pet me! H$$$$$$!");
-for (let i = 0; i <= 20; i++) {
- if (i % 2 !== 0) console,log(1);
-}
-
-const nums = [14,11,16,15,13,16,15,17,19,11,12,14,19,11,15,17,11,18,12,17,12,71,18,15,12];
-
-Expected output:
-=> 15
-
-
