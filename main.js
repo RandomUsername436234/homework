2290d2249357718f6c6a56f10713b8c54cb25c39
unknown <shafferjarrod99@aol.com>
2022-08-08 11:18:10 -0700

    Dom lab

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
-
-const toastTrigger = document.getElementById('liveToastBtn')
-const toastLiveExample = document.getElementById('liveToast')
-if (toastTrigger) {
-  toastTrigger.addEventListener('click', () => {
-    const toast = new bootstrap.Toast(toastLiveExample)
-
-    toast.show()
-  })
+console.log(mainE1)
+class MainE1 {
+    flexctr
 }
+
+class TopmenuE1 {
+     link; text
+}
+
+// Menu data structure
+var menuLinks = [
+    {text: 'about', href: '/about'},
+    {text: 'catalog', href: '/catalog'},
+    {text: 'orders', href: '/orders'},
+    {text: 'account', href: '/account'},
+  ];
