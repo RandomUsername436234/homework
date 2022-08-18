0fe47ebe91701cb7612133225bd469d2c730d383
unknown <shafferjarrod99@aol.com>
2022-08-18 09:56:00 -0700

    spacefolder

---
+++ Spacebattle/app.js
@@ -0,0 +1,53 @@
+function testFunction() {
+  console.log ("testFunction")  
+}
+testFunction()
+
+// Make array of 5 elements, a function to log each element and a way to run the function
+const array = [ "blue" , "Red" , "purple", "yellow", "orange" ];
+function elementfunction(informationtotakein){
+    console.log(informationtotakein);
+
+
+}
+elementfunction(array)
+
+let array = [ "Blue" , "sky" ];
+function blueElement(bluearrayelement){
+    console.log(bluearrayelement);
+
+
+}
+blueElementfunction(array)
+
+let array = [ "Red" , "Roses" ];
+function RedElement(Redarrayelement){
+    console.log (Redarrayelement);
+
+
+}
+RedElementfunction(array)
+
+let array = [ "purple" , "turtle" ];
+function purpleElement(purplearrayelement){
+    console.log(purplearrayElement);
+
+
+}
+purpleElementfunction(array)
+
+let array = [ "yellow" , "flowers" ];
+function yellowElement(yellowarrayelement){
+    console.log(yellowarrayelement);
+
+
+}
+yellowElementfunction(array)
+
+let array = [ "orange" , "fruit" ];
+function orangeElement (orangearrayelement){
+    console.log(orangearrayelement);
+
+
+}
+orangeElementfunction(array)
